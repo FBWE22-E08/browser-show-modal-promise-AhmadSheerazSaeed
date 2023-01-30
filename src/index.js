f46c126ec8f402e1d158b2d8dbef199cd67b9427
AhmadSheerazSaeed <khera724@gmail.com>
2023-01-30 13:57:17 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./css/style.css"; //importing css
+
+const myModal = document.querySelector(".modal");
+
+const close = document.querySelector(".close");
+
+const displayModelPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve();
+  }, 1000);
+});
+
+displayModelPromise.then((resolve) => {
+  myModal.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+  myModal.style.display = "none";
+});
